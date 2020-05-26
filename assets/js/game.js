e4e822c4ac6e390477bf6361cc168b14cf282830
Kushang Shah <kushangshah131@gmail.com>
2020-05-26 06:44:45 +0530

    Update game.js

--- assets/js/game.js
+++ assets/js/game.js
@@ -2,13 +2,20 @@ var config = {
         type: Phaser.AUTO,
         width: 800,
         height: 600,
-		physics: {
-			default: 'arcade',
-			arcade: {
-				gravity:{y:300},
-				debug: false
-			}
-		},
+	scale: {
+		mode: Phaser.Scale.FIT,
+		parent: 'phaser-example',
+		autoCenter: Phaser.Scale.CENTER_BOTH,
+		width: 800,
+		height: 600
+	},
+	physics: {
+		default: 'arcade',
+		arcade: {
+			gravity:{y:300},
+			debug: false
+		}
+	},
         scene: {
             preload: preload,
             create: create,
